6c51563d2f7d5fea014b881d24bcbe1014f53624
Ivan <aguirreivan96@gmail.com>
2022-01-11 10:54:02 -0600

    Added game module object

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const createPlayer = (name, marker) => {
+    console.log("createPlayer function is called");
     return {name, marker};
 }
 
@@ -11,7 +12,22 @@ const gameBoard = (() => {
     //convert squares to an Array
     const boardArray = Array.from(squares);
     // console.log(boardArray);
+    console.log("gameBoard function is working");
+
+    // boardArray.forEach(element => {
+    //     element.addEventListener("click", (e) =>{
+    //         console.log("You clicked Square ");
+    //     })
+    // })
 
     return {boardArray};
+
 })();
 
+const game = (() =>{
+    console.log("Game function is working");
+
+    // const player1 = createPlayer("Player 1", "X");
+    // const player2 = createPlayer("Player 2", "O");
+
+})();
