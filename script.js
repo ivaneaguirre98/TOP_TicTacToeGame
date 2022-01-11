66391fa22b5cabe65e67143113f520b870a8ae7f
Ivan <aguirreivan96@gmail.com>
2022-01-10 21:26:21 -0600

    Stored gameboard into a module

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("It works");
+const createPlayer = (name, marker) => {
+    return {name, marker};
+}
+
+
+const gameBoard = (() => {
+    // query selecting all elements that have a box class
+    let squares = document.querySelectorAll(".box");
+    // console.log(squares);
+
+    //convert squares to an Array
+    const boardArray = Array.from(squares);
+    // console.log(boardArray);
+
+    return {boardArray};
+})();
+
