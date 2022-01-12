f9122a49bffe5778dc16d39727e0c7e769066d2f
Ivan <aguirreivan96@gmail.com>
2022-01-11 21:13:03 -0600

    Added a checkGame function that checks for a winner

--- script.js
+++ script.js
@@ -15,12 +15,14 @@ const gameBoard = (() => {
     console.log("gameBoard function is working");
 
     boardArray.forEach(element => {
-        element.addEventListener("click", (e) =>{
+        element.addEventListener("click", (e, index) =>{
             //calls function that enters marker to the gameboard
             game.enterMarker(e.target);
 
             //calls function that switches player after marker is entered
             game.switchPlayer();
+
+            game.checkGame(e.target);
 
         }, {once:true}); //this limits event firing to one click. After click, it will no longer fire
     });
@@ -37,13 +39,11 @@ const game = (() =>{
     let activePlayer = player1;
 
     function enterMarker(target){
-        console.log(target);
         target.innerHTML = activePlayer.marker;
     }
 
     function switchPlayer(){
         // console.log("Switch Player function");
-        console.log(activePlayer);
         if(activePlayer === player1){
             activePlayer = player2;
         }
@@ -53,10 +53,42 @@ const game = (() =>{
         }
     }
 
+    function checkGame(target){
+        console.log("CheckGameFunctionCalled")
+        const winningConditions  = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+
+        for(let i = 0; i <= 7; i++){
+            const winCondition = winningConditions[i];
+            const a = gameBoard.boardArray[winCondition[0]].innerHTML;
+            const b = gameBoard.boardArray[winCondition[1]].innerHTML;
+            const c = gameBoard.boardArray[winCondition[2]].innerHTML;
+
+            if(a === "" || b === "" || c === ""){
+                continue;
+            }
+
+            if(a === b && b === c){
+                console.log("Round Won!");
+                break;
+            }
+        }
+
+    }
+
     return {
         activePlayer,
         switchPlayer,
-        enterMarker
+        enterMarker,
+        checkGame
     }
 
 })();
