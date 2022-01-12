dd220c24e02d6b33c6da9a7d14045ee821f4b8cd
Ivan <aguirreivan96@gmail.com>
2022-01-11 20:33:30 -0600

    Added enterMarker function and switchPlayer function that allows users to enter their marker and switch player afterwards.

--- script.js
+++ script.js
@@ -16,10 +16,14 @@ const gameBoard = (() => {
 
     boardArray.forEach(element => {
         element.addEventListener("click", (e) =>{
-            console.log("You clicked Square ");
-        })
-    })
+            //calls function that enters marker to the gameboard
+            game.enterMarker(e.target);
 
+            //calls function that switches player after marker is entered
+            game.switchPlayer();
+
+        }, {once:true}); //this limits event firing to one click. After click, it will no longer fire
+    });
 
     return {boardArray};
 
@@ -27,7 +31,32 @@ const gameBoard = (() => {
 
 const game = (() =>{
     console.log("Game function is working");
-    // const player1 = createPlayer("Player 1", "X");
-    // const player2 = createPlayer("Player 2", "O");
+    const player1 = createPlayer("Player 1", "X");
+    const player2 = createPlayer("Player 2", "O");
+
+    let activePlayer = player1;
+
+    function enterMarker(target){
+        console.log(target);
+        target.innerHTML = activePlayer.marker;
+    }
+
+    function switchPlayer(){
+        // console.log("Switch Player function");
+        console.log(activePlayer);
+        if(activePlayer === player1){
+            activePlayer = player2;
+        }
+
+        else{
+            activePlayer = player1;
+        }
+    }
+
+    return {
+        activePlayer,
+        switchPlayer,
+        enterMarker
+    }
 
 })();
